74b200abf3c4a7545b852994b6045dcb6264a9b2
vilko <helgi8@mail.ru>
2018-10-09 05:07:47 +0300

    Добавьте файлы проекта.

---
+++ Using_var/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Using_var
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 5.0; // ← исправьте эту строку
+            a += 0.5;
+            Console.WriteLine(a);
+        }
+    }
+}
